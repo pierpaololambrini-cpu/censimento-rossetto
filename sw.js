79a819d1b3d122127d75ec5ab105a6cba6e19886
Pierpaolo Lambrini <pierpaololambrini@Mac.fritz.box>
2026-05-28 19:33:23 +0200

    Censimento EAS – Gruppo Rossetto v1
    23 PdV senza CKP, province VR/RO/VI/PD/VE/BS/MN/CR/RE/MO.
    PWA con service worker e manifest.

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'censimento-eas-rossetto-v1';
+const PRECACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const net = fetch(e.request).then(resp => {
+        if (resp && resp.ok) {
+          caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+        }
+        return resp;
+      }).catch(() => cached);
+      return cached || net;
+    })
+  );
+});
